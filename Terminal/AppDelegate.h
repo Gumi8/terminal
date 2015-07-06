7dec78d31dec7acda86b8f4caa3eedc1a61f972b
gumi8 <gulmira.astana@gmail.com>
2015-07-06 23:09:34 +0600

    Initial Commit

---
+++ Terminal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Terminal
+//
+//  Created by 123 on 06/07/15.
+//  Copyright (c) 2015 Terminal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Terminal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Terminal
+//
+//  Created by 123 on 06/07/15.
+//  Copyright (c) 2015 Terminal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
